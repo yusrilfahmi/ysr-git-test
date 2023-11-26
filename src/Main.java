926afe95eead1eb7383c1798d48d96fb751df36b
yusrilfahmi <ysrfahmi@gmail.com>
2023-11-26 21:23:28 +0700

    Menambahkan method print dan code psvm di Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public void print(String nama){
+        System.out.println(nama);
+    }
+
+    public static void main(String[] args) {
+        Main objName = new Main();
+        objName.print("Yusril Fahmi");
+    }
 }
